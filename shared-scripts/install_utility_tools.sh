437e23f6a2a2e37e31af24e200872f7351b67eac
bkovaluk <bkovaluk@gmail.com>
2024-09-27 11:47:48 -0400

    updated source_image

--- shared-scripts/install_utility_tools.sh
+++ shared-scripts/install_utility_tools.sh
@@ -4,6 +4,6 @@ set -euo pipefail
 
 echo "Installing debugging tools..."
 
-sudo yum install -y curl-minimal wget less vim
+dnf install -y curl-minimal wget less vim
 
 echo "Debugging tools installed successfully."
