95acd7a8056e95fedabc0192067b7fc50af18374
bkovaluk <bkovaluk@gmail.com>
2024-09-27 12:23:21 -0400

    updated git workflow

--- images/common-build/scripts/install_gradle.sh
+++ images/common-build/scripts/install_gradle.sh
@@ -6,7 +6,9 @@ GRADLE_VERSION="${GRADLE_VERSION:-7.5.1}"
 
 echo "Installing Gradle version $GRADLE_VERSION..."
 
-dnf install gradle $GRADLE_VERSION
+dnf list available | grep gradle
+
+dnf install gradle # $GRADLE_VERSION
 
 # Verify installation
 gradle -v
